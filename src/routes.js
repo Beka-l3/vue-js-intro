83544cd0247c6ded957ad00c97ee2deab2c60be0
Beka-l3 <b.talgat@innopolis.university>
2023-10-06 13:12:45 +0300

    Initial nested routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
 import PageViewer from './views/PageViewer.vue';
 import CreatePage from './views/CreatePage.vue';
+import Pages from './views/Pages.vue';
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: '/:index?', component: PageViewer, props: true },
-        { path: '/pages', component: Pages },
-
-
-        { path: '/createPage', component: CreatePage }
+        { 
+            path: '/pages', 
+            component: Pages ,
+            children: [
+                { path: 'create', component: CreatePage }
+            ]
+        }
     ]
 })
 
